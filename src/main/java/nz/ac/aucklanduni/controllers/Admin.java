1b78570393d70f3f2bbd3164e1ace88c3c1a58f5
Bert Huang <bert.huang@outlook.com>
2015-03-24 18:10:27 +1300

    Setup server build to integrate with heroku / postgres / jetty. @mark-watkin.

---
+++ src/main/java/nz/ac/aucklanduni/controllers/Admin.java
@@ -0,0 +1,22 @@
+package nz.ac.aucklanduni.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.Mapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class Admin {
+
+    @RequestMapping(value = { "/" }, method = RequestMethod.GET)
+    public ModelAndView welcomePage() {
+
+        ModelAndView model = new ModelAndView();
+        model.setViewName("admin");
+        return model;
+
+    }
+
+}
