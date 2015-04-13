1d0ef251dd45225771ef81617b2ced854b0f3435
Bert Huang <bert.huang@outlook.com>
2015-04-13 14:14:32 +1200

    Added autoincrement to PK. Changed type int to type Integer. @mark-watkin.

--- src/main/resources/sql/creation_script.sql
+++ src/main/resources/sql/creation_script.sql
@@ -6,52 +6,43 @@
 CREATE DATABASE eye_atlas;
 \c eye_atlas
 
-
-
-
-
 CREATE TABLE entity (
-	id int NOT NULL PRIMARY KEY,
-	title varchar(100) NOT NULL,
-	description varchar(500) NOT NULL
+	id SERIAL PRIMARY KEY,
+	title VARCHAR(100) UNIQUE NOT NULL,
+	description VARCHAR(500) NOT NULL
 );
 
-CREATE TABLE tags(
-	id int NOT NULL PRIMARY KEY,
-	name varchar(100) NOT NULL
+CREATE TABLE tags (
+	id SERIAL PRIMARY KEY,
+	name VARCHAR(100) NOT NULL
 );
 
 CREATE TABLE entity_tag (
-	E_id int REFERENCES entity(id),
-	T_id int REFERENCES tags(id)
+	E_id INTEGER REFERENCES entity(id),
+	T_id INTEGER REFERENCES tags(id)
 );
 
 
-CREATE TABLE category(
-	id int NOT NULL PRIMARY KEY,
-	name varchar(100) NOT NULL
+CREATE TABLE category (
+	id SERIAL PRIMARY KEY,
+	name VARCHAR(100) NOT NULL
 );
 
 CREATE TABLE entity_category (
-	E_id int REFERENCES entity(id),
-	C_id int REFERENCES category(id)
+	E_id INTEGER REFERENCES entity(id),
+	C_id INTEGER REFERENCES category(id)
 );
 
 CREATE TABLE category_subcategory (
-	C_id int REFERENCES category(id),
-	S_id int REFERENCES category(id)
+	C_id INTEGER REFERENCES category(id),
+	S_id INTEGER REFERENCES category(id)
 );
 
-
-
-
-
-
 CREATE TABLE image (
-	id int NOT NULL PRIMARY KEY,
-	resolution int NOT NULL,
-	column_count int NOT NULL,
-	row_count int NOT NULL,
-	E_id int REFERENCES entity(id)
+	id SERIAL PRIMARY KEY,
+	resolution INTEGER NOT NULL,
+	column_count INTEGER NOT NULL,
+	row_count INTEGER NOT NULL,
+	E_id INTEGER REFERENCES entity(id)
 );
 
